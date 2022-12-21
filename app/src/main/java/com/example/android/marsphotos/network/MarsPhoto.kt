0882bee1c47838343d7afd285be331772f4c4510
M-fadel <m.ohm.md2008@hotmail.com>
2022-12-21 14:33:28 +0300

    commit

--- app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsPhoto.kt
@@ -2,9 +2,8 @@ package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Json
 
-class MarsPhoto {
+
     data class MarsPhoto(
         val id: String, @Json(name = "img_src") val imgSrcUrl: String
 
     )
-}
